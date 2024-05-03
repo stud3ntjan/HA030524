6d045087d90f0fd5238ae3de0985a03a11ee2ebc
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-03 16:05:52 +0200

    middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,22 @@
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const AccessTokens = require("../services/auth/AccessToken");
+
 function authMiddleWare(req, res, next) {
   // Middleware soll Access Token auslesen und die dekodierten Informationen an den jeweiligen Endpunkt weitergeben
+  const token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(StatusCodes.FORBIDDEN).send("No Token provided!");
+  }
+  try {
+    const decodedAccessToken = AccessTokens.decodeAccessToken(token);
+    req.user = decodedAccessToken; // { userId: 1 }
+  } catch (e) {
+    return res
+      .status(StatusCodes.FORBIDDEN)
+      .send("Something is wrong with your token");
+  }
+
+  return next();
 }
 
 module.exports = authMiddleWare;
