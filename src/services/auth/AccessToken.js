09d25baa5486efb214aba15c4ac85acf91db64a9
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-03 14:22:57 +0200

    Update AccessToken.js

--- src/services/auth/AccessToken.js
+++ src/services/auth/AccessToken.js
@@ -4,10 +4,16 @@ const { JWT_SECRET } = process.env;
 
 function createAccessToken(userId) {
   // Access Token mit dem Inhalt der User Id erstellen
+  const accessToken = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "24h" });
+
+  return accessToken;
 }
 
 function decodeAccessToken(accessToken) {
   // Den Inhalt des Access tokens dekodieren
+  const decodedToken = jwt.verify(accessToken, JWT_SECRET);
+
+  return decodedToken;
 }
 
 module.exports = { createAccessToken, decodeAccessToken };
