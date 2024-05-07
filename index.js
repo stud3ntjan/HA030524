ee763351951dcde88342023668e6a5e0a5b4df68
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-07 12:55:25 +0200

    impl winston

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+const winston = require("winston");
 const todoSequelize = require("./src/database/setup/database");
 const app = require("./src/server");
+
+require("dotenv").config();
 
 // Zugriff auf Umgebungsvariablen
 // const { PORT } = process.env;
 const PORT = process.env.PORT;
 
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info", // Log-Level
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
 todoSequelize
   .sync()
   .then(() => {
-    console.log("DB has been successfully initialized");
+    logger.info("DB has been successfully initialized");
   })
   .catch((e) => {
-    console.log(e);
+    logger.error(`Error initializing DB: ${e.message}`);
   });
 
 // App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  logger.info(`Server is running on port ${process.env.PORT}`);
 });
